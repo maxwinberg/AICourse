a1469a2e32b995c10468ea5f3be772134c947c2d
Max Winberg <winberg.max@gmail.com>
2016-09-02 14:56:38 +0200

    Started the homework

--- src/HMM1.java
+++ src/HMM1.java
@@ -2,5 +2,13 @@
  * Created by zwq on 2016/9/2.
  */
 public class HMM1 {
-    
+
+    public void function(){
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Lets rock AI!");
+    }
 }
